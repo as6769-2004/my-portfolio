b04e30c01435e5a96c88c70e2e1d976b8ce86071
as6769-2004 <adityasahar9991@gmail.com>
2025-07-13 09:58:25 +0530

    Configure Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
           200: '#ddd6fe',
           300: '#c4b5fd',
           400: '#a78bfa',
-          500: '#a259ec', // main purple
+          500: '#a259ec', 
           600: '#7c3aed',
           700: '#6d28d9',
           800: '#5b21b6',
